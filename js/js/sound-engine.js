9d8261e16d77039120e9c9b7401f326e960f2ba8
Instamod88 <132873133+Instamod88@users.noreply.github.com>
2026-01-16 17:51:20 -0300

    Excluir sound-engine.js

--- js/js/sound-engine.js
+++
@@ -1,8 +0,0 @@
-const sounds = {
-  click: new Audio("assets/sounds/click.mp3"),
-  generate: new Audio("assets/sounds/generate.mp3")
-};
-
-function playSound(name) {
-  if (sounds[name]) sounds[name].play();
-}
