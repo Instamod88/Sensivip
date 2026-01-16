3814fc6610324144c5c36360dfa488bcef636260
Instamod88 <132873133+Instamod88@users.noreply.github.com>
2026-01-16 16:47:18 -0300

    Atualizar o login.js

--- login.js
+++ login.js
@@ -2,18 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const botao = document.getElementById("btnEntrar");
   const erro = document.getElementById("error");
 
-  if (!botao) return;
-
-  botao.addEventListener("click", function (e) {
-    e.preventDefault();
-
+  botao.addEventListener("click", function () {
     const usuario = document.getElementById("username").value;
     const senha = document.getElementById("password").value;
 
     if (usuario === "Oliveira.2ss" && senha === "hs70%") {
+      localStorage.setItem("auth", "true");
       window.location.href = "dashboard.html";
     } else {
       erro.innerText = "Usuário ou senha incorretos";
     }
   });
-});
+})
